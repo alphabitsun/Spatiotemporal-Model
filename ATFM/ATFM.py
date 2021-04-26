ae51c79154d74706817bc869a74a42427b6a0188
1010626008 <43678022+1010626008@users.noreply.github.com>
2021-04-26 19:54:03 +0800

    ATFM

---
+++ ATFM/ATFM.py
@@ -0,0 +1,16 @@
+"""
+    Attentive Traffic Flow Machines
+
+"""
+
+import tensorflow as tf 
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Add, Concatenate, Conv2, ConvLSTM2D
+
+
+def ATFM():
+
+
+    
+    model = Model()
+    return model
