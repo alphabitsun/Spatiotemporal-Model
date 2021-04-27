2fc4b20e2e8a4a0bd296628d00b873368f797e0a
1010626008 <43678022+1010626008@users.noreply.github.com>
2021-04-27 13:08:09 +0800

    ATFM

---
+++ ATFM/ACFM.py
@@ -0,0 +1,16 @@
+"""
+    Attentive Crowd Flow Machines
+    
+"""
+
+import tensorflow as tf 
+from tensorflow.keras import Input, Model
+from tensorflow.keras.layers import Add, Concatenate, Conv2, ConvLSTM2D
+
+
+def ACFM():
+
+
+    
+    model = Model()
+    return model
--- ATFM/ATFM.py
+++ ATFM/ATFM.py
@@ -10,7 +10,12 @@ from tensorflow.keras.layers import Add, Concatenate, Conv2, ConvLSTM2D
 
 def ATFM():
 
+    # S_in:(n, h, w) 当前时刻的前 n 个时刻数据   -->  S_f:(c, h, w)
+    # P_in:(m, h, w) 前 m 天同一时刻的数据 -->  P_f:(c, h, w)
 
+
+    # step 1:
+     
     
     model = Model()
     return model
